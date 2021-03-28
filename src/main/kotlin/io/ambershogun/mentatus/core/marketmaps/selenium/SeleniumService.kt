7db8fe53c286c391f3dc15e5f0dfdc063fe27b85
Mikhail Fedotov <m.fedotov@cardsmobile.ru>
2021-03-28 19:05:42 +0300

    add implicit wait before taking screenshot

--- src/main/kotlin/io/ambershogun/mentatus/core/marketmaps/selenium/SeleniumService.kt
+++ src/main/kotlin/io/ambershogun/mentatus/core/marketmaps/selenium/SeleniumService.kt
@@ -1,6 +1,7 @@
 package io.ambershogun.mentatus.core.marketmaps.selenium
 
 import org.apache.commons.io.FileUtils
+import org.openqa.selenium.By
 import org.openqa.selenium.OutputType
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
@@ -32,10 +33,9 @@ class SeleniumService(
             chromeOptions.addArguments("--headless")
             chromeOptions.addArguments("user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36")
 
-
             driver = ChromeDriver(chromeOptions)
 
-            driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS)
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
 
             updateSectorsScreenshot()
             updateRegionsScreenshot()
@@ -48,15 +48,15 @@ class SeleniumService(
 
     private fun updateSectorsScreenshot() {
         driver.get("https://finviz.com/map.ashx")
-//        val map = driver.findElement(By.id("body"))
-        val mapFile = driver.getScreenshotAs(OutputType.FILE)
+        val map = driver.findElement(By.id("body"))
+        val mapFile = map.getScreenshotAs(OutputType.FILE)
         FileUtils.copyFile(mapFile, File("$marketMapsDir/sectors.png"))
     }
 
     private fun updateRegionsScreenshot() {
         driver.get("https://finviz.com/map.ashx?t=geo")
-//        val map = driver.findElement(By.id("body"))
-        val mapFile = driver.getScreenshotAs(OutputType.FILE)
+        val map = driver.findElement(By.id("body"))
+        val mapFile = map.getScreenshotAs(OutputType.FILE)
         FileUtils.copyFile(mapFile, File("$marketMapsDir/regions.png"))
     }
 }
